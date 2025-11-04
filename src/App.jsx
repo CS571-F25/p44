ce1728af51d27e98849e852ebfbd2f1c5722e86d
PedalQuotion85 <68934356+PedalQuotion85@users.noreply.github.com>
2025-11-04 01:05:01 -0600

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { HashRouter, Route, Routes } from 'react-router'
+import './App.css'
+import AboutMe from './components/AboutMe'
+import Home from './components/Home'
+
+
+function App() {
+
+  return <HashRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}></Route>
+      <Route path="/about" element={<AboutMe/>}></Route>
+    </Routes>
+  </HashRouter>
+
+}
+
+export default App
---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,7 @@
+export default function AboutMe(props){
+
+    return <div>
+        <h1>About me</h1>
+    </div>
+
+}
