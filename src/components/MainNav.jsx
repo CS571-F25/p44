91b70c828632410d3dfa484c6c8718974f49d215
Sun Oh <oh84@wisc.edu>
2025-11-19 21:04:51 -0600

    Made nav bars

---
+++ src/components/MainNav.jsx
@@ -0,0 +1,21 @@
+import { Navbar, Nav, Container } from "react-bootstrap";
+import { NavLink } from "react-router-dom";
+
+export default function MainNav() {
+  return (
+    <Navbar bg="light" expand="lg" fixed="top" className="mb-3">
+      <Container>
+        <Navbar.Brand as={NavLink} to="/">XZSO</Navbar.Brand>
+        <Navbar.Toggle aria-controls="main-nav" />
+        <Navbar.Collapse id="main-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={NavLink} to="/">Home</Nav.Link>
+            <Nav.Link as={NavLink} to="/game">Game</Nav.Link>
+            <Nav.Link as={NavLink} to="/devlog">Dev Log</Nav.Link>
+            <Nav.Link as={NavLink} to="/about">About Us</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+}
---
+++ src/pages/AboutUs.jsx
@@ -0,0 +1,19 @@
+export default function AboutUs() {
+  return (
+    <div className="p-3 text-center">
+      <h1>About Us</h1>
+      <p>
+        We are a small team working on a simple 3D web game for our CS571 project.
+        Our team members are Xiao and Sun.
+      </p>
+      <p>
+        Xiao is carrying this project by making all the important components of the game.
+        Sun focuses on the UI, trying to make a page that is worth looking at.
+      </p>
+      <p>
+        Our goal is to create a small but polished web game that runs smoothly in the browser.
+        It was inspired by Vampire Survivors, and MegaBonk.
+      </p>
+    </div>
+  );
+}
